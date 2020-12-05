7b2eb0d7c20db04ec0342034c013bf678276998a
Isabelle Villasenor <villasenori@outlook.com>
2020-12-05 09:11:39 -0700

    Add microbrewery seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Microbrewery.destroy_all
+Microbrewery.create!(
+  name: "Tuatara",
+  location: "7 Sheffield Street, Paraparaumu 5032, New Zealand",
+  license_renewal: '2020-12-04',
+  outdoor_seating: true,
+  social_distancing: true
+  )
+Microbrewery.create!(
+  name: "Parrot Dog",
+  location: "60/66 Kingsford Smith Street, Lyall Bay, Wellington 6022, New Zealand",
+  license_renewal: '2021-12-04',
+  outdoor_seating: true,
+  social_distancing: true
+  )
