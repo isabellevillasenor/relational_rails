3cdb286c513a9d69701a022dc098452008619422
Isabelle Villasenor <villasenori@outlook.com>
2020-12-07 17:02:25 -0700

    90% complete teas_user_story_4

--- app/controllers/teas_controller.rb
+++ app/controllers/teas_controller.rb
@@ -9,4 +9,19 @@ class TeasController < ApplicationController
     @teahouse = Teahouse.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    binding.pry
+    @teahouse = Teahouse.find(params[:id])
+    @tea = @teahouse.teas.create(tea_params)
+    redirect_to '/teahouses/:id/teas'
+  end
+
+  private
+  def tea_params
+    params.permit(:name, :category, :origin, :grams_needed, :steep_time, :brew_temp, :number_of_infusions, :season_picked, :ideal_teapot, :caffeinated)
+  end
+
 end
